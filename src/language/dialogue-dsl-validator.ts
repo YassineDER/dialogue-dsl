99d3cbff1701ed78b8f20f4f527a8fc64e530664
Yassine DERGAOUI <E238898X@etu.univ-nantes.fr>
2024-12-10 16:42:04 +0100

    initial commit

---
+++ src/language/dialogue-dsl-validator.ts
@@ -0,0 +1,31 @@
+import type { ValidationAcceptor, ValidationChecks } from 'langium';
+import type { DialogueDslAstType, Person } from './generated/ast.js';
+import type { DialogueDslServices } from './dialogue-dsl-module.js';
+
+/**
+ * Register custom validation checks.
+ */
+export function registerValidationChecks(services: DialogueDslServices) {
+    const registry = services.validation.ValidationRegistry;
+    const validator = services.validation.DialogueDslValidator;
+    const checks: ValidationChecks<DialogueDslAstType> = {
+        Person: validator.checkPersonStartsWithCapital
+    };
+    registry.register(checks, validator);
+}
+
+/**
+ * Implementation of custom validations.
+ */
+export class DialogueDslValidator {
+
+    checkPersonStartsWithCapital(person: Person, accept: ValidationAcceptor): void {
+        if (person.name) {
+            const firstChar = person.name.substring(0, 1);
+            if (firstChar.toUpperCase() !== firstChar) {
+                accept('warning', 'Person name should start with a capital.', { node: person, property: 'name' });
+            }
+        }
+    }
+
+}
